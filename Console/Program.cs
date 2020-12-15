1582c1ca40a1c29bab7f3a2f7d540ccd92aee3cf
Mikheil Skhirtladze <m.sxirtladze@gmail.com>
2020-12-15 12:59:46 +0400

    implement list

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Jotia
 {
@@ -7,13 +8,39 @@ namespace Jotia
         static void Main(string[] args)
         {
             var commad = Console.ReadLine();
+            var notes = new List<Note> {
+                new Note("today's tasks"),
+                new Note("coding ideas"),
+            };
 
             while (commad != "exit")
             {
                 //logic
+                if (commad == "list")
+                {
+                    // display all notes
+                    foreach (var note in notes)
+                    {
+                       Console.WriteLine($"- {note.GetTitle()}");
+                    }
+                }
 
                 commad = Console.ReadLine();
             }
+        }
+    }
+
+    internal class Note
+    {
+        private string title;
+        public Note(string title)
+        {
+            this.title = title;
+        }
+
+        public string GetTitle()
+        {
+            return title;
         }
     }
 }
